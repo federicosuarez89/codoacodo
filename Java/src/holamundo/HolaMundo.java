f5d000b509e3e67caa347091eaf343f4cb95cccf
Your Name <you@example.com>
2022-11-18 12:43:37 -0300

    Variables y tipos de datos en Java (Clase 2)

--- Java/src/holamundo/HolaMundo.java
+++ Java/src/holamundo/HolaMundo.java
@@ -3,5 +3,13 @@ package holamundo;
 public class HolaMundo {
     public static void main(String[] args) {
         System.out.println("Hola Mundo");
+
+        //Tipos de variables
+        //Area de definicion de variables
+        int edad=36;
+        String nombre="Jose Maria";
+        System.out.println(edad);
+        System.out.println(nombre);
+        System.out.println(nombre+" tiene "+edad+" años");
     }
 }
