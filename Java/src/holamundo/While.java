cda501b6ccdc6b80a9b88c7f71dee16401732119
Your Name <you@example.com>
2022-11-18 13:39:41 -0300

    Bucle while en Java (Clase 5)

---
+++ Java/src/holamundo/While.java
@@ -0,0 +1,13 @@
+package holamundo;
+
+public class While {
+    public static void main(String[] args) {
+        int c=1;
+        while (c<=5){
+            System.out.println(c);
+            c++;
+        }
+        System.out.println("Fin del bucle");
+
+    }
+}
