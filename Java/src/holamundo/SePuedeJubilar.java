7363a3606e9bc2a18c5c6930a04aee90a2f4db06
Your Name <you@example.com>
2022-11-18 13:36:05 -0300

    Ejercicio se puede jubilar en Java (Clase 4)

--- Java/src/holamundo/SePuedeJubilar.java
+++ Java/src/holamundo/SePuedeJubilar.java
@@ -1,10 +1,28 @@
 package holamundo;
+
+import java.util.Scanner;
+
 /*
     Se puede jubilar si es hombre y tiene 65 o mas,o si es mujer y tiene 60 o mas
  */
 public class SePuedeJubilar {
     public static void main(String[] args) {
-        int varon
+        String genero;
+        int edad;
+        Scanner entrada= new Scanner(System.in);
+        System.out.println("Ingrese el genero de la persona: ");
+         genero = entrada.nextLine();
+        System.out.println("Ingrese la edad de la persona: ");
+        edad=entrada.nextInt();
+        if (genero.equalsIgnoreCase("m")&& edad>=65) {
+            System.out.println("Se puede jubilar");
+        }   else if (genero.equalsIgnoreCase("f")&& edad>=60){
+            System.out.println("Se puede jubilar");
+            }
+        else {
+            System.out.println("No se puede jubilar");
+            }
+
 
     }
 }
